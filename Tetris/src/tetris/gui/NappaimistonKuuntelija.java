b4c88d245f56ddc65eb5b8b7ef5b75ac6c5cafc1
Topi <topi@topi-desktop.(none)>
2012-12-19 00:17:37 +0200

    .jar lisätty

---
+++ Tetris/src/tetris/gui/NappaimistonKuuntelija.java
@@ -0,0 +1,43 @@
+
+package tetris.gui;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import tetris.Ohjain;
+import tetris.domain.Suunta;
+
+
+public class NappaimistonKuuntelija implements KeyListener {
+    private Ohjain ohjain;
+
+    NappaimistonKuuntelija(Ohjain ohjain) {
+        this.ohjain = ohjain;
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            ohjain.siirraPalikkaa(Suunta.VASEN);
+           
+        }
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+            ohjain.siirraPalikkaa(Suunta.OIKEA);
+        }
+        if(e.getKeyCode() == KeyEvent.VK_DOWN){
+            ohjain.siirraPalikkaa(Suunta.ALAS);
+        }
+        if(e.getKeyCode() == KeyEvent.VK_UP){
+            ohjain.kaannaPalikka();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        
+    }
+    
+}
